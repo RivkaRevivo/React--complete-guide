b42885ca477ec09d396238ce623727571f2d3be9
Rivka Revivo <rivkar@etrogsystems.com>
2023-03-30 09:57:18 +0300

    section 66 done

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  const classes = 'card ' + props.className;
+  const classes = 'card ' + props.className;// a constant way to achive the clases property
 
   return <div className={classes}>{props.children}</div>;
 };
